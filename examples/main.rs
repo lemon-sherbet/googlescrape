f02767e9292e5d364a34a6b52ad8f05350f271da
lemon sherbet <41162747+lemon-sherbet@users.noreply.github.com>
2019-05-30 16:47:39 +0200

    make the example work with more than 1 argument

--- examples/main.rs
+++ examples/main.rs
@@ -1,7 +1,6 @@
 #![cfg(not(feature = "ffi"))]
 extern crate googlescrape;
-use std::env;
 
 fn main() {
-    println!("{:?}", googlescrape::google(&env::args().last().expect("Missing query").trim()).unwrap());
+    println!("{:?}", googlescrape::google(&(if std::env::args().len() > 1 { std::env::args().skip(1).collect::<Vec<String>>().join(" ") } else { panic!("\x1B[31m\n googel search query?\n\x1B[0m") })).unwrap());
 }
